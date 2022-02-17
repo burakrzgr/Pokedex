d6901e4693e8e23a2479aef68f5eed08be2ec356
Burak <burakrzgr@gmail.com>
2022-02-17 15:10:30 +0300

    rating sistemi tamamlandı

--- src/ui/Rating.js
+++ src/ui/Rating.js
@@ -2,45 +2,24 @@ import React, { useEffect } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleUp, faAngleDown } from '@fortawesome/free-solid-svg-icons'
 import { Row, Col, Button, ButtonGroup } from "react-bootstrap";
-import { fetchPokemon, patchPokemon } from '../axios/pokeserver'
+function Rating({ rating, pokeRate, id, showRate }) {
 
-function Rating({ rating, pokeNo }) {
-    
-  const [_rating, setRating] = React.useState(rating); // reducere cevrilmeli 
-  console.log("lol");
-  console.log(rating);
-  console.log(_rating);
-     const rate = (v) => {
-          var ts = (pokeNo ? fetchPokemon(pokeNo) : undefined);
-          ts.then(function (rs) {
-               var t = {};
-               if (v > 0) {
-                    t = { "id": pokeNo, "Rating": { "Total": rs.data.Rating.Total + 1, "Score": rs.data.Rating.Score + 1 } }
-               }
-               else {
-                    t = { "id": pokeNo, "Rating": { "Total": rs.data.Rating.Total + 1, "Score": rs.data.Rating.Score } };
-               }
-               patchPokemon(t);
-               _rating = { score: t.Rating.Score, total: t.Rating.Total };
-               setRating(_rating);
-          });
+     useEffect(() => {
+          showRate(id);
+     }, [id]);
 
-     };
-
-     
- //console.log(pokeNo);
      return (
           <div>
                <Row>
                     <Col xs={2} className={"pt-2"}>
-                         Rating : {rating != undefined ? rating.score < 0 ? ' ^-^ ' : parseFloat((rating.score / rating.total) * 10).toFixed(2) : 'Connection Error!'}
+                         Rating : {rating ? rating.score < 0 ? ' ^-^ ' : parseFloat((rating.score / rating.total) * 10).toFixed(2) : 'Connection Error!'}
                     </Col>
                     <Col xs={2}>
                          <ButtonGroup variant="contained" >
-                              <Button variant="contained" className={"btn-success"} onClick={(v) => rate(+1)}>
+                              <Button variant="contained" className={"btn-success"} onClick={() => {pokeRate(+1,id)}}>
                                    <FontAwesomeIcon icon={faAngleUp} />
                               </Button>
-                              <Button variant="outlined" className={"btn-danger"} onClick={(v) => rate(-1)} >
+                              <Button variant="outlined" className={"btn-danger"} onClick={() => {pokeRate(-1,id)}} >
                                    <FontAwesomeIcon icon={faAngleDown} />
                               </Button>
                          </ButtonGroup>
--- src/ui/popup.js
+++ src/ui/popup.js
@@ -1,27 +1,36 @@
 import { Modal, Container, Row, Col } from "react-bootstrap";
-import React, { useEffect } from "react";
-import '../button.css';
-import Evolution from './evolution';
-import PokeType from './poketype';
+import React, { useReducer } from "react";
+import reducer from "../reducers";
+import "../button.css";
+import Evolution from "./evolution";
+import PokeType from "./poketype";
 import { fetchPokemon, patchPokemon } from '../axios/pokeserver'
-import Rating from './Rating'
+import Rating from "./Rating";
 
 function Popup({ data, show, handleClose, setModalShow }) {
-  const poke = (data && data.No ? fetchPokemon(data.No) : undefined);
 
-  var rating = {};
+  const [pokeRating, setPokeRating] = useReducer(reducer, {
+    id: data.No,
+    total: 1,
+    score: 0,
+  });
+  const pokeRate = (v, id) => {
+    var poke = (id ? (fetchPokemon(id)) : undefined);
+    poke.then((rs) => {
+        var rating = rs.data && rs.data.Rating ? { id: id, score: rs.data.Rating.Score + (v > 0 ? 1 :0), total: rs.data.Rating.Total + 1} : { score: -1, total: 0 };
+        patchPokemon({id:id,"Rating":{"Score":rating.score,"Total":rating.total}})
+        setPokeRating({ type: "POKERATE", inf: rating });
+    }) ;
+  };
+  const showRate = (id) => {
+    var poke = (id ? (fetchPokemon(id)) : undefined);
+    poke.then((rs) => {
+        var rating = rs.data && rs.data.Rating ? { id: id, score: rs.data.Rating.Score , total: rs.data.Rating.Total} : { score: -1, total: 0 };
+        setPokeRating({ type: "POKERATE", inf: rating });
+    }) ;
+  };
 
-  if (poke) {
-    poke.then(function (rs) {
-      if (rs) {
-        ////// reducer ile burda update yapılmalı ; yoksa update edemiyor
-        rating = rs.data && rs.data.Rating ? { score: rs.data.Rating.Score, total: rs.data.Rating.Total } : { score: -1, total: 0 };
-      }
-      else {
-        console.log('Connection Error!!');
-      }
-    }).catch(error => { console.log(error) });
-  }
+
   return (
     <Modal
       show={show}
@@ -31,15 +40,30 @@ function Popup({ data, show, handleClose, setModalShow }) {
       backdrop="static"
       centered
     >
-      {data ?
+      {data ? (
         <Container style={{ backgroundColor: data.BColor }} fluid>
           <Row>
-            <Col md="auto" className="m-1" >
-              {data.Img ? <img src={require('../resource/img/' + data.Img)} style={{ height: '100%', width: '20rem', objectFit: 'contain' }} alt="Pokemon" /> : <div></div>}
+            <Col md="auto" className="m-1">
+              {data.Img ? (
+                <img
+                  src={require("../resource/img/" + data.Img)}
+                  style={{
+                    height: "100%",
+                    width: "20rem",
+                    objectFit: "contain",
+                  }}
+                  alt="Pokemon"
+                />
+              ) : (
+                <div></div>
+              )}
             </Col>
-            <Col >
-              <Modal.Header closeButton >
-                <Modal.Title id="contained-modal-title-vcenter" style={{ color: data.FColor }}>
+            <Col>
+              <Modal.Header closeButton>
+                <Modal.Title
+                  id="contained-modal-title-vcenter"
+                  style={{ color: data.FColor }}
+                >
                   {data.Name}
                 </Modal.Title>
               </Modal.Header>
@@ -48,20 +72,20 @@ function Popup({ data, show, handleClose, setModalShow }) {
                   <PokeType list={data.Type}></PokeType>
                 </h5>
                 <h6>Açıklama</h6>
-                <p>
-                  {data.Desc}
-                </p>
-                {console.log(rating)}
-                <Rating rating={rating} pokeNo={data.No} ></Rating>
+                <p>{data.Desc}</p>
+                <Rating rating={pokeRating} pokeRate={pokeRate} id={data.No} showRate={showRate}></Rating>
               </Modal.Body>
               <Modal.Footer className="justify-content-between">
                 <Evolution list={data.Prev} setModalShow={setModalShow} />
                 <div style={{ width: "30%" }}></div>
                 <Evolution list={data.Next} setModalShow={setModalShow} />
               </Modal.Footer>
-            </Col >
+            </Col>
           </Row>
-        </Container> : <div> Aradığnız pokemon kaydedilmedi!</div>}
+        </Container>
+      ) : (
+        <div> Aradığnız pokemon kaydedilmedi!</div>
+      )}
     </Modal>
   );
 }
