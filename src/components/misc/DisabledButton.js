2024169664724307ef12f684d774f2056cd6dd3c
Burak <burakrzgr@gmail.com>
2022-03-01 13:25:07 +0300

    update

--- src/components/misc/DisabledButton.js
+++
@@ -1,9 +0,0 @@
-
-import {Button} from "react-bootstrap";
-
-function DisabledButton({children,variant,OnClick,disabled}){
-    return(
-        <Button variant={variant} disabled={disabled} onClick={OnClick} >{children}</Button>
-    );
-
-}
