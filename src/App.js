df477701a7ba45b62035221ab9d4b913bc7a94c4
Burak <burakrzgr@gmail.com>
2022-03-03 14:09:39 +0300

    redirect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Search from './components/Seach';
-import Main from './components/Main';
-import LoadingPanel from './LoadingPanel'
-import LoginPanel from './LoginPanel'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Search from "./components/Seach";
+import Main from "./components/Main";
+import LoadingPanel from "./LoadingPanel";
+import LoginPanel from "./LoginPanel";
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 
 class App extends React.Component {
-
   render() {
     return (
       <div className="App">
@@ -16,8 +15,9 @@ class App extends React.Component {
         <header className="App-header">
           <BrowserRouter>
             <Routes>
-              <Route path="pokemons" element={<MainComp></MainComp>} />
-              <Route path="types" element={<div> Great!</div>} />
+              <Route path="/pokemons" element={<MainComp></MainComp>} />
+              <Route path="/types" element={<div> Great!</div>} />
+              <Route path="/region/:reg" element={<PokeRegion />} />
               <Route path="/" element={<MainComp></MainComp>} />
             </Routes>
           </BrowserRouter>
@@ -26,22 +26,37 @@ class App extends React.Component {
     );
   }
 }
+
+const PokeRegion =() => {
+  //let { path, url } = useRouteMatch();console.log(path);console.log(url);
+  var { reg } = useParams();
+  return (
+    <>
+    <div>Map of the Region! {reg}</div>
+    </>
+  );
+}
 function MainComp() {
-  const [state, setState] = useState({ showNewPoke: false, loading: true })
+  const [state, setState] = useState({ showNewPoke: false, loading: true });
   function setShowNewPoke(v) {
     setState({ ...state, showNewPoke: v });
-  };
+  }
   function loaded() {
     setState({ ...state, loading: false });
-  };
+  }
   return (
     <>
       <Search addPokeEvent={(v) => setShowNewPoke(v)}></Search>
-      {state.loading ?
-        <LoadingPanel isLoading={state.loading} loaded={() => loaded()} /> :
-        <Main addPokeEvent={(v) => setShowNewPoke(v)} showNewPoke={state.showNewPoke} ></Main>
-      }
-    </>);
-};
+      {state.loading ? (
+        <LoadingPanel isLoading={state.loading} loaded={() => loaded()} />
+      ) : (
+        <Main
+          addPokeEvent={(v) => setShowNewPoke(v)}
+          showNewPoke={state.showNewPoke}
+        ></Main>
+      )}
+    </>
+  );
+}
 
 export default App;
